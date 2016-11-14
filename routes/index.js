b024d33ea6366bbc6f2cc24a23ba5eb55532427c
= <joshua.raichur@gmail.com>
2016-11-14 16:26:37 -0600

    Add: Visual of twitter followers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var ejs = require('ejs');
 
 module.exports = function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('index.html', 'utf-8', function(err, content) {
+  fs.readFile('./index.html', 'utf-8', function(err, content) {
     if (err) {
       res.end('Error: ' + err);
       return;
--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ var T = new Twit(config);
 
 module.exports = function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('index.html', 'utf-8', function(err, content) {
+  fs.readFile('./index.html', 'utf-8', function(err, content) {
     if (err) {
       res.end('Error: ' + err);
       return;
@@ -16,11 +16,18 @@ module.exports = function(req, res){
     var twitter_data = [];
     twitter_data.username = "Person Not Found";
     T.get('followers/ids', { screen_name: req.params.user },  function (err, data, response) {
-      var users = "<ul>";
+      console.log(data);
+      var users = '{"nodes": [';
       for(var person in data.ids) {
-        users += "<li>" + data.ids[person] + "</li>";
+        console.log(data.ids[person]);
+        users += '{"name":"' + data.ids[person] + '", "group":1}';
+        if(person < data.ids.length-1) {
+          users += ",";
+        } else {
+          users += "],";
+        }
       }
-      users += "</ul>";
+      users += '"links":[{"source":2,"target":1,"weight":1},{"source":0,"target":2,"weight":3}]}';
       twitter_data.username = req.params.user;
       twitter_data.list = users;
       var html = ejs.render(content, {twitter_data: twitter_data});
